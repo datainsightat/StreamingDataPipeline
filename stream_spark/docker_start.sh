da5bcc0b5bbce54f4d347a74f23facd94a85ee43
BernhardMayrhofer <bernhard@mayrhofer.email>
2021-12-29 12:38:40 +0100

    Stable Kafka Cluster

--- stream_spark/docker_start.sh
+++ stream_spark/docker_start.sh
@@ -7,8 +7,8 @@
 #$SPARK_HOME/sbin/start-slave.sh spark://localhost:7077
 $SPARK_HOME/sbin/start-all.sh
 
-#Jupyter
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=
+# #Jupyter
+# jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token=
 
 #Keep Container alive Test
 tail -f /dev/null
